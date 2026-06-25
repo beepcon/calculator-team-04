2c11de81d17abcbaf5a7ccbdce0651252f60764b
beepcon <skkwon0801@gmail.com>
2026-06-25 14:54:22 +0900

    Feat: 숫자 검증 추가

--- ops/subtract.py
+++ ops/subtract.py
@@ -1,5 +1,6 @@
 def subtract(a, b):
-    # TODO: 두 수의 차를 반환하세요.
+    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+        return "숫자로 쳐주세요"
     return a - b
 
 # 우엥 뭐가 뭔지 모르겠어영 우엥 GGGGGG
