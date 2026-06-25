c120738eca55ec169fe4e0e293fa9cc74f0b6c7e
kdh <zldnfrjdi@naver.com>
2026-06-25 15:22:04 +0900

    Docs: divide 함수에 주석 추가

--- ops/divide.py
+++ ops/divide.py
@@ -1,5 +1,16 @@
 def divide(a, b):
-    # TODO: 두 수를 나누어 반환하세요.
+    """두 수를 나누어 반환하는 함수이다.
+
+    Args:
+        a (int): 나눠지는 수
+        b (int): 나누는 수
+
+    Raises:
+        ValueError: b가 0인 경우
+
+    Returns:
+        a를 b로 나눈 결과
+    """
     if b == 0:
         raise ValueError("0으로 나눌 수 없음")
     return a / b
